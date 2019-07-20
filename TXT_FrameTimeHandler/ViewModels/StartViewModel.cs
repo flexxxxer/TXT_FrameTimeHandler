48054c87b19694b0c65bad7e617a20293eccd5aa
Alex <bahx4pc@gmail.com>
2019-07-20 22:13:03 +0300

    update file structure

---
+++ TXT_FrameTimeHandler/ViewModels/StartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TXT_FrameTimeHandler.ViewModels
+{
+    public class StartViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
